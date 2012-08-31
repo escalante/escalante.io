e9b0033deb4c5213b96c9ba7098ffab1d4b01da1
Galder Zamarreño <galder@zamarreno.com>
2012-08-31 17:58:33 +0200

    Add downloads section and announce 0.1.0 release

--- _ext/downloads.rb
+++ _ext/downloads.rb
@@ -1,8 +1,7 @@
 
 class Downloads
 
-  #REPO_PREFIX        = "http://repository-projectodd.forge.cloudbees.com/staging/io/escalante"
-  REPO_PREFIX        = "http://repository-projectodd.forge.cloudbees.com/release/io/escalante"
+  REPO_PREFIX        = "https://repository.jboss.org/nexus/content/groups/public/io/escalante/"
 
   def initialize(enabled=true)
     @enabled = enabled
@@ -13,16 +12,16 @@ class Downloads
 
     site.releases.each do |release|
       release.urls ||= OpenStruct.new
-      release.urls.docs ||= OpenStruct.new
-      release.urls.docs.browse = "#{site.base_url}/docs/#{release.version}/"
+      #release.urls.docs ||= OpenStruct.new
+      #release.urls.docs.browse = "#{site.base_url}/docs/#{release.version}/"
 
-      release.urls.jira = "https://jira.jboss.org/jira/secure/IssueNavigator.jspa?reset=true&jqlQuery=project=IMMUTANT+AND+fixVersion=#{release.jira_version}&sorter/field=issuekey&sorter/order=DESC"
+      release.urls.jira = "https://jira.jboss.org/jira/secure/IssueNavigator.jspa?reset=true&jqlQuery=project=ESC+AND+fixVersion=#{release.jira_version}&sorter/field=issuekey&sorter/order=DESC"
 
       release.urls.github ||= OpenStruct.new
       release.urls.github.log = "https://github.com/escalante/escalante/commits/#{release.version}"
       release.urls.github.tree = "https://github.com/escalante/escalante/tree/#{release.version}"
 
-      release_suffix = "/escalante-dist/#{release.version}/escalante-dist-#{release.version}-bin.zip"
+      release_suffix = "/escalante-dist/#{release.version}/escalante-dist-#{release.version}.zip"
       release.urls.remote_dist_zip  = "#{REPO_PREFIX}#{release_suffix}"
     end
   end
---
+++ _ext/release_helper.rb
@@ -0,0 +1,28 @@
+module ReleaseHelper
+
+  def release_for_version(version)
+    site.releases.find { |release| release.version == version }
+  end
+
+  def announcement_for_version(version)
+    site.posts.find { |p| p.version == version }
+  end
+
+  def latest_release
+    site.releases.first
+  end
+
+  def latest_release?(release)
+    release == latest_release
+  end
+
+  #def doc_chapter_for_version(version, chapter)
+  #  "/documentation/#{version}/#{chapter}.html"
+  #end
+  #
+  #def doc_bundle_name(release)
+  #  release.version == '0.1.0' ? "immutant-docs-#{release.version}.jar" :
+  #    "immutant-docs-#{release.version}-bin.zip"
+  #end
+
+end
